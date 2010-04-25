3572be6fad5e8118f0d6aed698e6ba4fa01ee3fb
Gabriel Gayan <gabrielgayan@gmail.com>
2010-04-25 17:27:40 -0400

    reescribo operaciones de suma y resta de vectores con factor w

--- patch.py
+++ patch.py
@@ -1,5 +1,3 @@
-from punto import *
-
 #implementado como triangulo
 class Patch:
 
@@ -16,11 +14,15 @@ class Patch:
 
     #retorna el baricentro del parche
     def center(self):
-        x = (self.p1.x + self.p2.x + self.p3.x) / 3
-        y = (self.p1.y + self.p2.y + self.p3.y) / 3
-        z = (self.p1.z + self.p2.z + self.p3.z) / 3
+        #x = (self.p1.x + self.p2.x + self.p3.x) / 3
+        #y = (self.p1.y + self.p2.y + self.p3.y) / 3
+        #z = (self.p1.z + self.p2.z + self.p3.z) / 3
         
-        return Punto(x, y, z)
+        #return Punto(x, y, z)
+        return self.p1.suma(self.p2.suma(self.p3,1),3) # == (p1 + ((p2 + p3) / 1)) / 3
         
+    #no es necesario normalizar, basta la direccion
     def normal(self):
-        return cruz(self.p1, self.p2)
+        b_menos_a = self.p2.resta(self.p1, 1) # == (p2 - p1) / 1
+        c_menos_a = self.p3.resta(self.p1, 1) # == (p3 - p1) / 1
+        return b_menos_a.cruz(c_menos_a)
--- punto.py
+++ punto.py
@@ -4,11 +4,31 @@ class Punto:
         self.x = x1
         self.y = y1
         self.z = z1
+    
+        #w es el factor de correcion
+    def suma(self, p2, w):
+        if w == 0:
+            w = 1
         
-def cruz(p1, p2):
-    #calculo los puntos segun la notacion matricial
-    x = p1.y * p2.z - p1.z * p2.y
-    y = p1.z * p2.x - p1.x * p2.z
-    z = p1.x * p2.y - p1.y * p2.x
-    #y lo retornamos como punto
-    return Punto(x, y, z)
+        x = (self.x + p2.x) / w
+        y = (self.y + p2.y) / w
+        z = (self.z + p2.z) / w
+        return Punto(x, y, z)
+    
+    #w es el factor de correcion
+    def resta(self, p2, w):
+        if w == 0:
+            w = 1
+        
+        x = (self.x - p2.x) / w
+        y = (self.y - p2.y) / w
+        z = (self.z - p2.z) / w
+        return Punto(x, y, z)
+        
+    def cruz(self, p2):
+        #calculo los puntos segun la notacion matricial
+        x = self.y * p2.z - self.z * p2.y
+        y = self.z * p2.x - self.x * p2.z
+        z = self.x * p2.y - self.y * p2.x
+        #y lo retornamos como punto
+        return Punto(x, y, z)
