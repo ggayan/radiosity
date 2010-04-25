bcc16af85d3ce9df5435d52acc0bb61c267a0ab5
Gabriel Gayan <gabrielgayan@gmail.com>
2010-04-25 17:07:10 -0400

    agrego el producto cruz al archivo punto.py y defino la normal y el centro del parche

--- patch.py
+++ patch.py
@@ -1,18 +1,26 @@
+from punto import *
+
+#implementado como triangulo
 class Patch:
-	
+
     #x = 0
     #y = 0
     #z = 0
     r = 0  # reflectividad
     e = 0  # emisividad
-	
+
     def __init__(self,p1,p2,p3):
         self.p1 = p1
         self.p2 = p2
         self.p3 = p3
 
-    def center():
-        return 2
-    
-    def normal():
-        return 2
+    #retorna el baricentro del parche
+    def center(self):
+        x = (self.p1.x + self.p2.x + self.p3.x) / 3
+        y = (self.p1.y + self.p2.y + self.p3.y) / 3
+        z = (self.p1.z + self.p2.z + self.p3.z) / 3
+        
+        return Punto(x, y, z)
+        
+    def normal(self):
+        return cruz(self.p1, self.p2)
--- punto.py
+++ punto.py
@@ -4,3 +4,11 @@ class Punto:
         self.x = x1
         self.y = y1
         self.z = z1
+        
+def cruz(p1, p2):
+    #calculo los puntos segun la notacion matricial
+    x = p1.y * p2.z - p1.z * p2.y
+    y = p1.z * p2.x - p1.x * p2.z
+    z = p1.x * p2.y - p1.y * p2.x
+    #y lo retornamos como punto
+    return Punto(x, y, z)
